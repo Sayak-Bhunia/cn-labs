675b2eacaa318eb468c2e70ccf09ae9cddb1c3df
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2025-06-08 14:47:47 +0530

    Update MyClient.java

--- both-way/MyClient.java
+++ both-way/MyClient.java
@@ -3,26 +3,21 @@ import java.io.*;
 
 class Client {
     public static void main(String[] args) throws Exception {
-        try {
-            Socket s = new Socket("localhost", 6666);
-            DataInputStream din = new DataInputStream(s.getInputStream());
-            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            String s1 = "", s2 = "";
-            while(!s1.equals("stop")) {
-                System.out.println("Client = ");
-                s2 = br.readLine();
-                dout.writeUTF(s2);
-                dout.flush();
-                s2 = din.readUTF();
-           	    System.out.println("Server = "+s2);
-            }
-            dout.close();
-    	    din.close();
-            s.close();
-        } 
-        catch (Exception e) {
-            System.out.println(e);
+        Socket s = new Socket("localhost", 6666);
+        DataInputStream din = new DataInputStream(s.getInputStream());
+        DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s1 = "", s2 = "";
+        while(!s1.equals("stop")) {
+            System.out.println("Client = ");
+            s2 = br.readLine();
+            dout.writeUTF(s2);
+            dout.flush();
+            s2 = din.readUTF();
+       	    System.out.println("Server = "+s2);
         }
+        dout.close();
+	din.close();
+        s.close();
     }
 }
