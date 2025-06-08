06110a39c3741a0d1d8a26bb3b5664b4f5c944a3
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2025-06-08 14:42:21 +0530

    Update MyClient.java

--- both-way/MyClient.java
+++ both-way/MyClient.java
@@ -1,32 +1,27 @@
-import java.io.*;
 import java.net.*;
+import java.io.*;
 
-public class MyClient {
-    public static void main(String[] args) {
+class Client {
+    public static void main(String[] args) throws Exception {
         try {
             Socket s = new Socket("localhost", 6666);
-            
             DataInputStream din = new DataInputStream(s.getInputStream());
             DataOutputStream dout = new DataOutputStream(s.getOutputStream());
-            
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            
-            String str = "", str2 = "";
-            
-            while (!str.equals("stop")) {
-                System.out.print("Client: ");
-                str = br.readLine();
-                dout.writeUTF(str);
+            String s1 = "", s2 = "";
+            while(!s1.equals("stop")) {
+                System.out.println("Client = ");
+                s2 = br.readLine();
+                dout.writeUTF(s2);
                 dout.flush();
-                
-                str2 = din.readUTF();
-                System.out.println("Server says: " + str2);
+                s2 = din.readUTF();
+           	    System.out.println("Server = "+s2);
             }
-            
             dout.close();
-            din.close();
+    	    din.close();
             s.close();
-        } catch (Exception e) {
+        } 
+        catch (Exception e) {
             System.out.println(e);
         }
     }
