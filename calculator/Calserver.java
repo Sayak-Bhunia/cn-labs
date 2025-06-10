f8ee33789ee026315f6d5f2ad89cc09fcda59a89
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2025-06-11 00:42:30 +0530

    Update Calserver.java

--- calculator/Calserver.java
+++ calculator/Calserver.java
@@ -4,28 +4,22 @@ import java.util.*;
 
 public class Calserver {
     public static void main(String[] args) throws IOException {
-        // Create a server socket on port 6666
         ServerSocket server = new ServerSocket(6666);
         System.out.println("Server is running... Waiting for a client to connect...");
 
-        // Accept a client connection
         Socket sock = server.accept();
         System.out.println("Client connected.");
 
-        // Input and output streams
         DataInputStream inpStrm = new DataInputStream(sock.getInputStream());
         DataOutputStream outpStrm = new DataOutputStream(sock.getOutputStream());
 
-        // Scanner for server-side input (currently used to enter operands manually)
         Scanner sc = new Scanner(System.in);
 
         try {
             while (true) {
-                // Read operation choice from client
                 int oprtr = inpStrm.readInt();
                 System.out.println("Client has requested operation: " + oprtr);
 
-                // Get operands from server-side input (NOTE: ideally, client should send these)
                 System.out.print("Enter first number: ");
                 int data1 = sc.nextInt();
                 System.out.print("Enter second number: ");
@@ -33,22 +27,22 @@ public class Calserver {
 
                 int res = 0;
                 switch (oprtr) {
-                    case 1: // Addition
+                    case 1:
                         res = data1 + data2;
                         outpStrm.writeUTF(Integer.toString(res));
                         break;
 
-                    case 2: // Subtraction
+                    case 2:
                         res = data1 - data2;
                         outpStrm.writeUTF(Integer.toString(res));
                         break;
 
-                    case 3: // Multiplication
+                    case 3:
                         res = data1 * data2;
                         outpStrm.writeUTF(Integer.toString(res));
                         break;
 
-                    case 4: // Division
+                    case 4:
                         if (data2 == 0) {
                             outpStrm.writeUTF("Error: Division by zero");
                         } else {
@@ -57,7 +51,7 @@ public class Calserver {
                         }
                         break;
 
-                    default: // Invalid operation
+                    default:
                         outpStrm.writeUTF("You have given an invalid choice!");
                         break;
                 }
@@ -67,7 +61,6 @@ public class Calserver {
         } catch (Exception exp) {
             System.out.println("An error occurred: " + exp.getMessage());
         } finally {
-            // Cleanup resources
             sc.close();
             inpStrm.close();
             outpStrm.close();
@@ -77,18 +70,3 @@ public class Calserver {
         }
     }
 }
-
-
-C:\Users\student\Desktop\sayak>java Calserver
-Server is running... Waiting for a client to connect...
-Client connected.
-Client has requested operation: 1
-Enter first number: 5
-Enter second number: 10
-Result sent to the client...
-
-
-
-
-
-
